f00efbc2b1d467a1110e608055d1964926f32faf
Zhiling <zhliao@ethz.ch>
2025-11-14 09:07:40 +0100

    Update mesh files

--- 10_massmovement/massmovement.py
+++ 10_massmovement/massmovement.py
@@ -51,7 +51,8 @@ my_factory   = TwoRegionBasinFactory(width=flume_length, height=flume_width, mid
 # Set the matID
 # Add region markers
 my_factory.regions = [
-    RegionMarker((b+flume_length/2)/2, 0, max_area=15, attribute=0),
+    RegionMarker( (b+flume_length/2)/2, 0, max_area=15, attribute=0),
+    RegionMarker(-(b+flume_length/2)/2, 0, max_area=15, attribute=0),
     RegionMarker(                   0, 0, max_area=15, attribute=1)
 ]
 
